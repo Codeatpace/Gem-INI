addbbefd10973c63df9e1f8b91e8eeba2c919ba0
SakshiSingh2211 <sakshi.s@tantrasoftsolutions.com>
2024-10-08 10:16:23 +0530

    recentChange

--- app.js
+++ app.js
@@ -19,3 +19,41 @@ app.use('/api', Search);
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
+
+
+
+
+
+
+
+
+// import './App.css';
+
+// function App() {
+//   const myfunc = async () => {
+//     const response = await fetch('http://localhost:5000/api/Search',  {
+//       method: "POST",
+//       headers:{
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({name: "singh"})
+//     })
+//     const json = await response.json();
+//     console.log(json)
+//     console.log("Func called")
+//   }
+//   return (
+//    <>
+//       <div className="body">
+//         <br/><br/>
+//         <h2>🙂 Welcome to GEM - INI 🙂</h2>
+//         <div className="quesSide">
+//           <label>Enter Question: </label><input type="text" /><br/>
+//           <input type='Submit' placeholder='Submit' onClick={() => myfunc()}/>
+//         </div>
+//       </div>
+//    </>
+//   );
+// }
+
+// export default App;
